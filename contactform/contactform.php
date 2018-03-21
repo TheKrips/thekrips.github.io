976ca1b07c7926135d4462208c12984c82032b0e
TheKrips <xkrips@gmail.com>
2018-03-21 18:36:00 +0200

    comm new bg

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,2 +1,31 @@
 <?php 
 var_dump($_POST);
+
+
+
+$name = $_POST["name"];
+$telnumber = $_POST["email"];
+ 
+$EmailTo = "xkrips@gmail.com";
+$Subject = "ЗАЯВКА НА ТУР";
+ 
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Номер телефона: ";
+$Body .= $telnumber;
+$Body .= "\n";
+ 
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+ 
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+ 
+?>
